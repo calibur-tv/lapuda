628a3f85cb61bcc80edf6850ffb5eecc621f25e9
Box <2300206764@qq.com>
2017-11-13 14:15:16 +0800

    切换SDK到轮子王版本

--- config/app.php
+++ config/app.php
@@ -183,6 +183,7 @@ return [
         Laravel\Scout\ScoutServiceProvider::class,
         ScoutEngines\Elasticsearch\ElasticsearchProvider::class,
         Barryvdh\Cors\ServiceProvider::class,
+        Overtrue\LaravelFilesystem\Qiniu\QiniuStorageServiceProvider::class,
     ],
 
     /*
