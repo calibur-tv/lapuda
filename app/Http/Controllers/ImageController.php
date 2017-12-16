e07c933a58ac91bcdc32663ad2e921d595ed31d4
falstack <icesilt@outlook.com>
2017-12-16 09:07:50 +0800

    update uptoken

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,16 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\Image\UpTokenRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class ImageController extends Controller
 {
-    public function token(UpTokenRequest $request)
+    public function token(Request $request)
     {
+        $setKey = $request->get('setKey');
+
+        if ($setKey)
+        {
+            $validator = Validator::make($request->all(), [
+                'modal' => [
+                    'required',
+                    Rule::in(['user', 'bangumi', 'post']),
+                ],
+                'type' => 'required|string',
+                'id' => 'required|integer'
+            ]);
+
+            if ($validator->fails())
+            {
+                return $this->resErr(['参数校验失败']);
+            }
+        }
+
         $auth = new \App\Services\Qiniu\Auth();
 
-        $key = "{$request->get('modal')}/{$request->get('type')}/{$request->get('id')}/".time();
+        $key = $setKey
+            ? "{$request->get('modal')}/{$request->get('type')}/{$request->get('id')}/".time()
+            : null;
+
         $token = $auth->uploadToken($key);
 
         return $this->resOK([
