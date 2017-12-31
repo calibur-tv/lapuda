f6140d40703a2ed00f8eac3ea85afcfbcddd8c7d
falstack <icesilt@outlook.com>
2017-12-31 15:50:43 +0800

    fixed: post show page 1

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -118,10 +118,10 @@ class PostRepository extends Repository
         /**
          * 因为：page = 1 的时候，不用获取 1 楼
          * 所以，当 take = 10 时：
-         * page = 1 -> start = 1 end = 9
+         * page = 1 -> start = 0 end = 9
          * page = 2 -> start = 10, end = 19
          */
-        $start = $page === 1 ? 1 : ($page - 1) * $take;
+        $start = $page === 1 ? 0 : ($page - 1) * $take;
         $stop = $page === 1 ? $take - 1 : $start + $take - 1;
 
         if ($postMasterId)
