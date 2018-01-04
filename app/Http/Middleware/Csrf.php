d59162c5ce08b5ad6cf406537752c895132149bc
falstack <icesilt@outlook.com>
2018-01-04 08:47:48 +0800

    add: app name & app version

--- app/Http/Middleware/Csrf.php
+++ app/Http/Middleware/Csrf.php
@@ -3,15 +3,22 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Log;
 
 class Csrf
 {
-    protected $allows = [
+    protected $domains = [
         'https://www.calibur.tv',
         'https://m.calibur.tv',
         '127.0.0.1',
-        ''
+    ];
+
+    protected $apps = [
+        'innocence',
+        'geass'
+    ];
+
+    protected $versions = [
+
     ];
 
     protected $except = [];
@@ -21,7 +28,7 @@ class Csrf
         if (
             config('app.env') !== 'production' ||
             in_array($request->method(), ['HEAD', 'GET', 'OPTIONS']) ||
-            in_array($request->headers->get('Origin'), $this->allows) ||
+            in_array($request->headers->get('Origin'), $this->domains) ||
             in_array($request->url(), $this->except) ||
             md5(config('app.md5_salt') . $request->headers->get('X-Auth-Timestamp')) === $request->headers->get('X-Auth-Token')
         ) {
