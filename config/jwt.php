b4aabd5c7064c626cc8beeeb3f70b12f085fa3d8
falstack <icesilt@outlook.com>
2018-01-23 08:10:34 +0800

    update: jwt

--- config/jwt.php
+++ config/jwt.php
@@ -38,7 +38,7 @@ return [
     |
     */
 
-    'ttl' => 40320,
+    'ttl' => 60,
 
     /*
     |--------------------------------------------------------------------------
@@ -115,7 +115,7 @@ return [
     |
     */
 
-    'blacklist_enabled' => env('JWT_BLACKLIST_ENABLED', true),
+    'blacklist_enabled' => env('JWT_BLACKLIST_ENABLED', false),
 
     /*
     |--------------------------------------------------------------------------
