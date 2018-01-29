35d4892ad50365f87c6fb0c82d04cbe495e02112
falstack <icesilt@outlook.com>
2018-01-29 21:56:30 +0800

    update: host

--- app/Http/Middleware/Csrf.php
+++ app/Http/Middleware/Csrf.php
@@ -9,6 +9,8 @@ class Csrf
     protected $domains = [
         'https://www.calibur.tv',
         'https://m.calibur.tv',
+        'https://t-www.calibur.tv',
+        'https://t-m.calibur.tv',
         ''
     ];
 
@@ -35,6 +37,7 @@ class Csrf
             in_array($request->headers->get('Origin'), $this->domains) ||
             in_array($request->url(), $this->except) ||
             (
+                time() - intval($request->headers->get('X-Auth-Timestamp')) < 60 &&
                 md5(config('app.md5_salt') . $request->headers->get('X-Auth-Timestamp')) === $request->headers->get('X-Auth-Token')
             )
         ) {
