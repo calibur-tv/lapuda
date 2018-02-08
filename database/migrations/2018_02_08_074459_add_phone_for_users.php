6a28a17415147a892e000dab0ede7a2576e5a172
falstack <icesilt@outlook.com>
2018-02-08 08:21:43 +0800

    fixed: phone

--- database/migrations/2018_02_08_074459_add_phone_for_users.php
+++ database/migrations/2018_02_08_074459_add_phone_for_users.php
@@ -14,7 +14,7 @@ class AddPhoneForUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedInteger('phone');
+            $table->unsignedInteger('phone')->nullable();
         });
     }
 
