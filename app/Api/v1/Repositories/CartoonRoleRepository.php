3c505702f2bdcb91af847ca95dd668e32c8a1453
falstack <icesilt@outlook.com>
2018-02-26 08:14:02 +0800

    add: cartoon role init

---
+++ app/Api/v1/Repositories/CartoonRoleRepository.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuistack
+ * Date: 2017/12/2
+ * Time: 下午12:49
+ */
+
+namespace App\Api\V1\Repositories;
+
+class CartoonRoleRepository extends Repository
+{
+
+}
---
+++ app/Api/v1/Transformers/CartoonRoleTransformer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuistack
+ * Date: 2018/1/5
+ * Time: 上午11:36
+ */
+
+namespace App\Api\V1\Transformers;
+
+
+class CartoonRoleTransformer extends Transformer
+{
+
+}
