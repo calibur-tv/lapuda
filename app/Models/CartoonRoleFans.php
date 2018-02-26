3f6c14913c4bbe5247dcbe01c57805978a22a5ef
falstack <icesilt@outlook.com>
2018-02-26 21:21:04 +0800

    update

--- app/Models/CartoonRoleFans.php
+++ app/Models/CartoonRoleFans.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class CartoonRoleFans extends Model
 {
-    //
+    protected $table = 'cartoon_role_fans';
+
+    protected $fillable = [
+        'user_id',
+        'role_id',
+        'star_count'
+    ];
 }
