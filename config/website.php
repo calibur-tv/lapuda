bf52b9ec6bfd302e92a7cc4297f73f67f87255de
falstack <icesilt@outlook.com>
2018-04-01 08:17:35 +0800

    add: baidu push

--- config/website.php
+++ config/website.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
+    'url' => 'https://www.calibur.tv/',
     'image' => 'https://image.calibur.tv/',
     'video' => env('APP_ENV') !== 'production' ? 'https://image.calibur.tv/' : 'https://video.calibur.tv/',
     'list_count' => 15,
+    'push_baidu_token' => env('PUSH_BAIDU_TOKEN')
 ];
