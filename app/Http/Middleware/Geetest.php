654c23d4cc787b5a0cfc492e2bdd342e291fe211
falstack <icesilt@outlook.com>
2018-06-04 21:43:50 +0800

    update

--- app/Http/Middleware/Geetest.php
+++ app/Http/Middleware/Geetest.php
@@ -9,16 +9,15 @@ class Geetest
     public function handle($request, Closure $next)
     {
         $geetest = $request->get('geetest');
-        $time = $geetest['access'];
-
-        if ((time() - $time) > 15)
-        {
-            return response([
-                'code' => 40001,
-                'message' => config('error.40001'),
-                'data' => '',
-            ], 400);
-        }
+//        $time = $geetest['access'];
+//        if ((time() - $time) > 15)
+//        {
+//            return response([
+//                'code' => 40001,
+//                'message' => config('error.40001'),
+//                'data' => '',
+//            ], 400);
+//        }
 
         if (md5(config('geetest.key') . $geetest['access']) === $geetest['secret'])
         {
