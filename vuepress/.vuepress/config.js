8543c2b4bbbe227e67653d9fd6a5a0c262f3acb0
falstack <icesilt@outlook.com>
2018-08-01 21:52:58 +0800

    update: vuepress

---
+++ vuepress/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  head: [
+    ['link', { rel: 'icon', href: 'https://static.calibur.tv/favicon.ico' }]
+  ],
+  port: 8088,
+  base: '/Laputa/',
+  dest: 'docs'
+}
