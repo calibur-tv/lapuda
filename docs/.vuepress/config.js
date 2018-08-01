e07ecedf381f912e7a31134a02c874ff4050a434
falstack <icesilt@outlook.com>
2018-08-01 20:30:23 +0800

    add: vuepress docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  head: [
+    ['link', { rel: 'icon', href: 'https://static.calibur.tv/favicon.ico' }]
+  ],
+  port: 8080
+}
