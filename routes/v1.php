0ae9633303a844ee91e24b6fb6aeb2b9c14dfdc1
falstack <icesilt@outlook.com>
2018-08-03 23:44:53 +0800

    add: image like migrate

--- routes/v1.php
+++ routes/v1.php
@@ -9,6 +9,8 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version(['v1', 'latest'], function ($api)
 {
+    $api->get('/migrate', 'App\Api\V1\Controllers\SearchController@migrate');
+
     $api->group(['prefix' => '/search'], function ($api)
     {
         $api->get('/new', 'App\Api\V1\Controllers\SearchController@search');
