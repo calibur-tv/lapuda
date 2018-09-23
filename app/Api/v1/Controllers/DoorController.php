aa923f4f99edfbd9ad416b7da48c630c4fda6f77
falstack <icesilt@outlook.com>
2018-09-23 23:18:29 +0800

    fixed: typo

--- app/Api/v1/Controllers/DoorController.php
+++ app/Api/v1/Controllers/DoorController.php
@@ -11,6 +11,7 @@ use App\Services\Sms\Message;
 use App\Api\V1\Repositories\Repository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
