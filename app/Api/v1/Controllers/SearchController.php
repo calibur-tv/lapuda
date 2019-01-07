def650d1cef0a7759a8f97a77884369c9860b892
falstack <icesilt@outlook.com>
2019-01-07 20:50:52 +0800

    remove migration code

--- app/Api/v1/Controllers/SearchController.php
+++ app/Api/v1/Controllers/SearchController.php
@@ -3,12 +3,8 @@
 namespace App\Api\V1\Controllers;
 
 use App\Api\V1\Repositories\BangumiRepository;
-use App\Models\Bangumi;
-use App\Models\Video;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Services\OpenSearch\Search;
-use Illuminate\Support\Facades\DB;
 use Mews\Purifier\Facades\Purifier;
 
 /**
@@ -69,115 +65,5 @@ class SearchController extends Controller
         $bangumiRepository = new BangumiRepository();
 
         return $this->resOK($bangumiRepository->searchAll());
-    }
-
-    public function migrate()
-    {
-        $bangumiIds = Bangumi
-            ::where('migration_state', 0)
-            ->pluck('id')
-            ->take(100)
-            ->toArray();
-
-        if (empty($bangumiIds))
-        {
-            return 'over';
-        }
-        $bangumiRepository = new BangumiRepository();
-        foreach ($bangumiIds as $bid)
-        {
-            $bangumi = $bangumiRepository->item($bid);
-            $videoObj = $bangumiRepository->videos($bid, json_decode($bangumi['season']));
-            $hasSeason = $videoObj['has_season'];
-            if ($hasSeason)
-            {
-                foreach ($videoObj['videos'] as $rank => $videoPackage)
-                {
-                    $videos = $videoPackage['data'];
-                    $videoIds = [];
-                    foreach ($videos as $video)
-                    {
-                        $videoIds[] = $video['id'];
-                    }
-                    $isLast = $rank === count($videoPackage) - 1;
-                    $seasonId = DB
-                        ::table('bangumi_seasons')
-                        ->insertGetId([
-                            'bangumi_id' => $bid,
-                            'name' => $videoPackage['name'],
-                            'rank' => $rank + 1,
-                            'summary' => $bangumi['summary'],
-                            'avatar' => $bangumi['avatar'],
-                            'other_site_video' => intval($bangumi['others_site_video']),
-                            'released_at' => $isLast ? $bangumi['released_at'] : 0,
-                            'released_time' => $isLast ? $bangumi['released_time'] : 0,
-                            'end' => $isLast ? intval($bangumi['end']) : 1,
-                            'published_at' => Carbon::createFromTimestamp(time($videoPackage['time']))->toDateTimeString(),
-                            'created_at' => $bangumi['created_at'],
-                            'updated_at' => $bangumi['created_at'],
-                            'videos' => empty($videoIds) ? '' : implode(',', $videoIds)
-                        ]);
-
-                    foreach ($videos as $video)
-                    {
-                        Video
-                            ::where('id', $video['id'])
-                            ->update([
-                                'bangumi_season_id' => $seasonId,
-                                'episode' => $video['part'] - $videoPackage['base']
-                            ]);
-                    }
-                }
-            }
-            else
-            {
-                $videos = $videoObj['videos'];
-                if (gettype($videos) === 'array')
-                {
-                    $videos = $videos[0];
-                }
-                $videoIds = [];
-                foreach ($videos['data'] as $video)
-                {
-                    $videoIds[] = $video['id'];
-                }
-
-                $seasonId = DB
-                    ::table('bangumi_seasons')
-                    ->insertGetId([
-                        'bangumi_id' => $bid,
-                        'name' => '',
-                        'rank' => 1,
-                        'summary' => $bangumi['summary'],
-                        'avatar' => $bangumi['avatar'],
-                        'other_site_video' => intval($bangumi['others_site_video']),
-                        'released_at' => $bangumi['released_at'],
-                        'released_time' => $bangumi['released_time'],
-                        'end' => intval($bangumi['end']),
-                        'published_at' => Carbon::createFromTimestamp($bangumi['published_at'])->toDateTimeString(),
-                        'created_at' => $bangumi['created_at'],
-                        'updated_at' => $bangumi['created_at'],
-                        'videos' => empty($videoIds) ? '' : implode(',', $videoIds)
-                    ]);
-
-                foreach ($videos['data'] as $video)
-                {
-                    Video
-                        ::where('id', $video['id'])
-                        ->update([
-                            'bangumi_season_id' => $seasonId,
-                            'episode' => $video['part'] - $videos['base']
-                        ]);
-                }
-            }
-            Bangumi
-                ::where('id', $bid)
-                ->update([
-                    'has_season' => $hasSeason,
-                    'migration_state' => 1
-                ]);
-        }
-
-        return 'success';
     }
 }
