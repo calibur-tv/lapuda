93d90a3153eabd7ebb367b7093fd7a7383255149
falstack <icesilt@outlook.com>
2019-01-28 00:02:52 +0800

    add: answer draft view role

--- app/Api/v1/Controllers/AnswerController.php
+++ app/Api/v1/Controllers/AnswerController.php
@@ -78,6 +78,11 @@ class AnswerController extends Controller
             {
                 return $this->resErrNotFound();
             }
+
+            if (!$answer['published_at'] && $answer['user_id'] !== $this->getAuthUserId())
+            {
+                return $this->resErrRole();
+            }
         }
 
         return $this->resOK([
