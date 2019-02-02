98762f579e4a173874a4b0fdb7d8760a28fa9c91
falstack <icesilt@outlook.com>
2019-02-02 10:44:17 +0800

    remove: migration code

--- app/Api/v1/Services/Trending/CartoonRoleTrendingService.php
+++ app/Api/v1/Services/Trending/CartoonRoleTrendingService.php
@@ -142,8 +142,6 @@ class CartoonRoleTrendingService extends TrendingService
                 $list[$i]['lover_nickname'] = $user['nickname'];
                 $list[$i]['lover_zone'] = $user['zone'];
             }
-            Redis::DEL('cartoon_role_fans_' . $role['id'] . '_star_count_total');
-            Redis::DEL('cartoon_role_fans_' . $role['id'] . '_fans_count_total');
         }
         $cartoonRoleStarCounter = new CartoonRoleStarCounter();
         $cartoonRoleFansCounter = new CartoonRoleFansCounter();
