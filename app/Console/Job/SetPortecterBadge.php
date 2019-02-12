0dd9fb63b3a8cc13d3394c15f4f9317f4a3b01ea
falstack <icesilt@outlook.com>
2019-02-12 12:10:56 +0800

    add: gift for lover

--- app/Console/Job/SetPortecterBadge.php
+++ app/Console/Job/SetPortecterBadge.php
@@ -12,6 +12,7 @@ namespace App\Console\Job;
 use App\Api\V1\Repositories\CartoonRoleRepository;
 use App\Api\V1\Repositories\UserRepository;
 use App\Api\V1\Services\Tag\Base\UserBadgeService;
+use App\Api\V1\Services\VirtualCoinService;
 use App\Models\CartoonRole;
 use App\Models\CartoonRoleFans;
 use Illuminate\Console\Command;
@@ -49,8 +50,8 @@ class SetPortecterBadge extends Command
         $i = 0;
         $userRepository = new UserRepository();
         $cartoonRoleRepository = new CartoonRoleRepository();
-        $userBadgeService = new UserBadgeService();
         $lovers = [];
+        $virtualCoinService = new VirtualCoinService();
         foreach ($list as $idol_id => $star_count)
         {
             $i++;
@@ -73,23 +74,8 @@ class SetPortecterBadge extends Command
             }
             if (!in_array($lover_id, $lovers))
             {
-                $userBadgeService->setUserBadge(
-                    $lover_id,
-                    6,
-                    "在初代守护者争夺战中，该用户为「{$idol['name']}」应援了「{$star}」个团子，使其位居偶像排行榜第「{$i}」位。此荣耀将永存于世，感谢有你，尊敬的守护者！"
-                );
+                $virtualCoinService->coinGift($lover_id, $star / 2);
                 $lovers[] = $lover_id;
-            }
-            $userIds = CartoonRoleFans
-                ::where('role_id', $idol_id)
-                ->where('user_id', '<>', $lover_id)
-                ->where('star_count', '>=', 30)
-                ->orderBy('star_count', 'DESC')
-                ->pluck('user_id')
-                ->toArray();
-            foreach ($userIds as $uid)
-            {
-                $userBadgeService->setUserBadge($uid, 7);
             }
         }
 
