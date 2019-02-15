8e1c07be3364f71264a32254dcdacf34806dce41
falstack <icesilt@outlook.com>
2019-02-15 08:47:27 +0800

    fixed: namespace

--- app/Console/Job/SaveIdolMarketPrice.php
+++ app/Console/Job/SaveIdolMarketPrice.php
@@ -10,6 +10,7 @@ namespace App\Console\Job;
 
 use App\Api\V1\Repositories\CartoonRoleRepository;
 use App\Models\CartoonRole;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redis;
